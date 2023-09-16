bd5963c05b40d21c75fa2c4113e063ea84f5832e
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2023-09-16 08:46:27 +0000

    Spacing er way off

--- scrap/rotation/rotation.js
+++ scrap/rotation/rotation.js
@@ -1,14 +1,95 @@
-/////////////////////////////////////////////////////////////////
-//    Sýnidæmi í Tölvugrafík
-//     Sýnir notkun á lyklaborðsatburðum til að hreyfa spaða
-//
-//    Hjálmtýr Hafsteinsson, september 2023
-/////////////////////////////////////////////////////////////////
 var canvas;
 var gl;
 
-var xmove;
-var ymove;
+var sideWalkBuffer;
+var colorA = vec4(0,0,0,1);
+var carBuffer;
+var colorB = vec4(1,0,0,1);
+var frogBuffer;
+var colorC = vec4(0,1,0,1)
+var vPosition;
+var vertices = [
+    vec2( 0,   -0.8 ),
+    vec2( 0.1, -0.9 ),
+    vec2(-0.1, -0.9 )];
+var sideWalk = [];
+var cars = [];
+var color;
+var carDir;
+var car1 = [];
+
+function generateCars(n){
+    for(let i = 0; i < n; i++){
+        cars.push(
+            // first street
+            // car1 = [
+            vec2(0.9+i*0.7, -0.6),
+            vec2(0.9+i*0.7, -0.7),
+            vec2(0.7+i*0.7, -0.6),
+            vec2(0.7+i*0.7, -0.6),
+            vec2(0.7+i*0.7, -0.7),
+            vec2(0.9+i*0.7, -0.7),
+            // ],
+            // second street
+            vec2(0.9+i*0.7, -0.4),
+            vec2(0.9+i*0.7, -0.5),
+            vec2(0.7+i*0.7, -0.5),
+            vec2(0.7+i*0.7, -0.5),
+            vec2(0.7+i*0.7, -0.4),
+            vec2(0.9+i*0.7, -0.4),
+
+            // third street
+            vec2(0.9+i*0.7, -0.2),
+            vec2(0.9+i*0.7, -0.3),
+            vec2(0.7+i*0.7, -0.2),
+            vec2(0.7+i*0.7, -0.2),
+            vec2(0.7+i*0.7, -0.3),
+            vec2(0.9+i*0.7, -0.3),
+
+            // fourth street
+            vec2(0.9+i*0.7, 0.2),
+            vec2(0.9+i*0.7, 0.3),
+            vec2(0.7+i*0.7, 0.2),
+            vec2(0.7+i*0.7, 0.2),
+            vec2(0.7+i*0.7, 0.3),
+            vec2(0.9+i*0.7, 0.3),
+
+            //fifth street
+            vec2(0.9+i*0.7, 0.4),
+            vec2(0.9+i*0.7, 0.5),
+            vec2(0.7+i*0.7, 0.5),
+            vec2(0.7+i*0.7, 0.5),
+            vec2(0.7+i*0.7, 0.4),
+            vec2(0.9+i*0.7, 0.4),
+
+            // sixth street
+            vec2(0.9+i*0.7, 0.6),
+            vec2(0.9+i*0.7, 0.7),
+            vec2(0.7+i*0.7, 0.6),
+            vec2(0.7+i*0.7, 0.6),
+            vec2(0.7+i*0.7, 0.7),
+            vec2(0.9+i*0.7, 0.7),
+        );
+    }
+    return cars;
+}
+
+function moveCars(offset){
+    for(let i = 0; i<cars.length; i++){
+        cars[i][0] = cars[i][0]+offset
+    }
+    for(let i = 0; i < cars.length; i+=6){
+        if(cars[i][0]+offset < -1 ){ //&& carDir == 1 
+            cars[i][0] = cars[i][0]+2.2
+            cars[i+1][0] = cars[i+1][0]+2.2
+            cars[i+2][0] = cars[i+2][0]+2.2
+            cars[i+3][0] = cars[i+3][0]+2.2
+            cars[i+4][0] = cars[i+4][0]+2.2
+            cars[i+5][0] = cars[i+5][0]+2.2
+        }
+    }
+    return cars;
+}
 
 window.onload = function init() {
 
@@ -17,6 +98,26 @@ window.onload = function init() {
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
     
+    sideWalk = [
+        vec2(-1, 1),
+        vec2(-1, 0.8),
+        vec2(1, 1),
+        vec2(-1, 0.8),
+        vec2(1, 0.8),
+        vec2(1, 1),//topsidewalk ends here  
+        vec2(-1, 0.1),  
+        vec2(-1, -0.1),  
+        vec2(1, 0.1),  
+        vec2(1, 0.1),  
+        vec2(1, -0.1),  
+        vec2(-1, -0.1),//midsidewalk ends here  
+        vec2(-1, -0.8),
+        vec2(-1, -1),
+        vec2(1, -0.8),
+        vec2(1, -0.8),
+        vec2(1, -1),
+        vec2(-1, -1),//bottomsidewalk ends here
+    ];
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
 
@@ -26,66 +127,91 @@ window.onload = function init() {
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
-    var vertices = [
-        vec2( 0,    -0.7 ),
-        vec2( -0.1, -0.9 ),
-        vec2( 0.1,  -0.9 )
-    ];
-    
     // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    sideWalkBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, sideWalkBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(sideWalk), gl.STATIC_DRAW );
+    
+    carBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, carBuffer);
+    generateCars(3);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(cars), gl.STATIC_DRAW );
+
+    frogBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, frogBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.DYNAMIC_DRAW );
 
     // Associate out shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.enableVertexAttribArray( vPosition );
 
+    // uniform breytur
+    color = gl.getUniformLocation(program, "vColor");
+
+    const step = 0.1;
+    var dir = 1;
     // Event listener for keyboard
-    window.addEventListener("keydown", function(e){
-        console.log(vertices)
-        xmove = 0
-        ymove = 0
-        switch( e.code ) {
-            case "ArrowUp":
-                ymove = 0.04;
-
-                break;
-            case "ArrowLeft":	// vinstri ör
-                xmove = -0.04;
-                break;
-            case "ArrowRight":	// hægri ör
-                xmove = 0.04;
-                
-                break;
-            case "ArrowDown":
-                ymove = -0.04;
-                break;
-            default:
-                xmove = 0.0;
-                ymove = 0.0;
+    window.addEventListener("keydown", (e) => {
+        if(e.code == "ArrowUp" && vertices[0][1]+step <= 1){
+            if(dir == 2){// var að benda niður
+                vertices[0][1] += 0.1;
+                vertices[1][1] -= 0.1;
+                vertices[2][1] -= 0.1;
+            }
+            for(let i = 0; i<3; i++) {
+                vertices[i][1] += step
+            }
+            dir = 1
         }
-        for(i=0; i<3; i++) {
-            vertices[i][0] += xmove;
-            console.log(ymove)
-            vertices[i][1] += ymove;
-            console.log(ymove)
-
+        else if(e.code == "ArrowRight" && vertices[0][0]+step <= 1) {
+            for(let i = 0; i<3; i++) {
+                vertices[i][0] += step 
+            }
         }
-
-        console.log(vertices)
+        else if(e.code == "ArrowDown" && vertices[0][1]-step >= -1){
+            if(dir == 1){// var að benda upp
+                vertices[0][1] -= 0.1;
+                vertices[1][1] += 0.1;
+                vertices[2][1] += 0.1;
+            }
+            for(let i = 0; i<3; i++) {
+                vertices[i][1] -= step
+            }
+            dir = 2
+        }
+        else if(e.code == "ArrowLeft" && vertices[0][0]-step >= -1){
+            for(let i = 0; i<3; i++) {
+                vertices[i][0] -= step;
+            }
+        }   
+        gl.bindBuffer(gl.ARRAY_BUFFER, frogBuffer); 
         gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
-    } );
-
+    });
     render();
 }
 
-
 function render() {
-    
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
 
+    //sideWalkBuffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, sideWalkBuffer);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0,0);
+    gl.uniform4fv(color, flatten(colorA));
+    gl.drawArrays( gl.TRIANGLES, 0, sideWalk.length);
+
+    //carBuffer
+    gl.bindBuffer( gl.ARRAY_BUFFER, carBuffer );    
+    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(cars));
+    moveCars(-0.01);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0,0);
+    gl.uniform4fv(color, flatten(colorB));
+    gl.drawArrays( gl.TRIANGLES, 0, cars.length );
+
+    //frogBuffer
+    gl.bindBuffer( gl.ARRAY_BUFFER, frogBuffer);
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0,0);
+    gl.uniform4fv(color, flatten(colorC));
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+ 
     window.requestAnimationFrame(render);
 }
--- v1/frogger.js
+++ v1/frogger.js
@@ -1,98 +1,232 @@
-var gl;
 var canvas;
+var gl;
 
 var colorFrog;
-var locFrog;
+var vPosition;
 
-//var a = vec2( 0,   -0.9 );
-//var b = vec2(-0.1, -1 );
-//var c = vec2( 0.1, -1 );
-//var vertices = [a,b,c];
-//var theta = 1.5708;
-//var x0 = (a[0]+b[0]+c[0])/3;
-//var y0 = (a[1]+b[1]+c[1])/3;
-
-var xmove;
-var ymove;
-
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-
-    var vertices = new Float32Array([-0.1, -1, 0, -0.8, 0.1, -1]);
-    vertUp = new Float32Array([-0.1, -0.1, -0.1, 0.1, 0.1, 0.0]);
-    vertDown = new Float32Array([0.1, 0.1, 0.1, -0.1, -0.1, 0.0]);
-
-    //  Configure WebGL
-
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.75, 1.0, 1.0, 1.0 );
-    
-    //  Load shaders and initialize attribute buffers
-    
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-    
-    // Load the data into the GPU
-    
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-
-    // Associate shader variables with our data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    colorFrog = gl.getUniformLocation( program, "color" );
-    locFrog = gl.getUniformLocation( program , "frog");
+// Spacing er off
+// Collision 
+// stig
 
 
-    window.addEventListener("keydown", function(e){
-        //console.log(vertices)
-        xmove = 0
-        ymove = 0
-        switch( e.code ) {
+// Svæðið er frá -maxX til maxX og -maxY til maxY
+var maxX = 1.0;
+var maxY = 1.0;
+
+var frog = [
+    vec2( 0,    -0.85 ),
+    vec2( -0.05, -0.95 ),
+    vec2( 0.05,  -0.95 )
+];
+
+var sidewalk = [
+    vec2(-1, 1),
+    vec2(-1, 0.8),
+    vec2(1, 1),
+    vec2(-1, 0.8),
+    vec2(1, 0.8),
+    vec2(1, 1), //top
+    vec2(-1, -0.8),
+    vec2(-1, -1),
+    vec2(1, -0.8),
+    vec2(1, -0.8),
+    vec2(1, -1),
+    vec2(-1, -1), //bottom
+];
+
+var totalHeight = maxY * 2; // maxY represents the maximum y-coordinate value
+var totalLanes = 7; // Total number of lanes (including sidewalks)
+var laneHeight = totalHeight / totalLanes;
+
+var currentLane = 0;
+
+
+
+//var LANES = 7; // Fjöldi akgreina (með gangstétt).
+//var laneSize;       // Stærð einnar akgreinar.
+var cars = [];
+var carWidth = 0.3;
+var carHeight = 0.15;
+var numCarsInLane = 5;
+var carSpeeds = [0.017, 0.014, 0.011, 0.008, 0.005];
+var carSpacing = 0.5;
+
+
+function generateCars() {
+    for (var lane = 1; lane < totalLanes; lane++) { // Start from lane 1
+        var laneY = maxY - lane * laneHeight - laneHeight / 2;
+        var totalWidth = numCarsInLane * carWidth + (numCarsInLane - 1) * carSpacing;
+        var initialCarX = maxX + (maxX - totalWidth) / 2; // Center the cars initially on the screen
+
+        for (var i = 0; i < numCarsInLane; i++) {
+            var carX = initialCarX + i * (carWidth + carSpacing);
+            cars.push({
+                position: vec2(carX, laneY),
+                width: carWidth,
+                height: carHeight,
+                speed: carSpeeds[lane - 1] // Adjust the speed based on lane
+            });
+        }
+    }
+}
+
+function moveCars() {
+    for (var i = 0; i < cars.length; i++) {
+        cars[i].position[0] -= cars[i].speed; // Corrected typo
+
+        // Check if a car moves off the screen to the left
+        if (cars[i].position[0] < -maxX - carWidth) {
+            // Reposition the car to the right edge of the screen
+            cars[i].position[0] = maxX + carSpacing;
+        }
+    }
+}
+
+
+function collision(){
+
+}
+
+function points(){
+
+}
+
+
+
+
+function movement(){
+    // Event listener for keyboard
+    window.addEventListener("keydown", function (e) {
+        xmove = 0;
+        ymove = 0;
+        switch (e.code) {
             case "ArrowUp":
-                ymove = 0.25;
-                //ctx.rotate(180*Math.PI/180)
+                if (currentLane < totalLanes - 1) { // Check if not already in the top lane
+                    ymove += laneHeight; // Move one lane up
+                    currentLane++;
+                }
                 break;
-            case "ArrowLeft":	// vinstri ör
-                xmove = -0.25;
+            case "ArrowLeft": // vinstri ör
+                xmove -= 0.1;
                 break;
-            case "ArrowRight":	// hægri ör
-                xmove = 0.25;
+            case "ArrowRight": // hægri ör
+                xmove += 0.1;
                 break;
             case "ArrowDown":
-                ymove = -0.25;
+                if (currentLane > 0) { // Check if not already in the bottom lane
+                    ymove -= laneHeight; // Move one lane down
+                    currentLane--;
+                }
                 break;
             default:
                 xmove = 0.0;
                 ymove = 0.0;
         }
-        for(i=0; i<3; i++) {
-            vertices[i][0] += xmove;
-            //console.log(ymove)
-            vertices[i][1] += ymove;
-            //console.log(ymove)
 
+        var newVertices = frog.map(function (vertex) {
+            var newX = vertex[0] + xmove;
+            var newY = vertex[1] + ymove;
+
+            return [newX, newY];
+        });
+
+        // Check if any part of the frog is outside the boundaries
+        var isOutsideBoundaries = newVertices.some(function (vertex) {
+            return (
+                vertex[0] < -maxX ||
+                vertex[0] > maxX ||
+                vertex[1] < -maxY ||
+                vertex[1] > maxY
+            );
+        });
+
+        // Only update the frog's position if it's not outside the boundaries
+        if (!isOutsideBoundaries) {
+            frog = newVertices;
+            gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(frog));
         }
-        
-        gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(vertices));
     });
+}
+
+window.onload = function init()
+{
+    var canvas = document.getElementById("gl-canvas");
+    
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.2, 0.2, 0.2, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    colorFrog = gl.getUniformLocation( program, "vColor" );
+
+    movement();
+
+    generateCars();
 
     render();
 
 }
 
+function renderSidewalk(){
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(sidewalk), gl.STATIC_DRAW );
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0,0);
+    gl.uniform4fv( colorFrog, vec4(0.8,0.8,0.8, 1));
+    gl.drawArrays( gl.TRIANGLES, 0, sidewalk.length);
+}
+
+function renderCars(){
+    for (var i = 0; i < cars.length; i++) {
+        var car = cars[i];
+        gl.bufferData(gl.ARRAY_BUFFER, flatten([
+            vec2(car.position[0] - car.width / 2, car.position[1] + car.height / 2),
+            vec2(car.position[0] - car.width / 2, car.position[1] - car.height / 2),
+            vec2(car.position[0] + car.width / 2, car.position[1] + car.height / 2),
+            vec2(car.position[0] + car.width / 2, car.position[1] + car.height / 2),
+            vec2(car.position[0] - car.width / 2, car.position[1] - car.height / 2),
+            vec2(car.position[0] + car.width / 2, car.position[1] - car.height / 2)
+        ]), gl.STATIC_DRAW );
+        gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+        gl.uniform4fv(colorFrog, vec4(1.0, 0.0, 0.0, 1));
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+}
+
+function renderFrog(){
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(frog), gl.DYNAMIC_DRAW);
+    gl.uniform4fv(colorFrog, vec4(0,0.5,0, 1));
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
 
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.uniform4fv( colorFrog, vec4(0,0.5,0, 1) );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    renderSidewalk();
+
+    moveCars(); 
+
+    renderCars();
+
+    renderFrog();
 
     window.requestAnimationFrame(render);
 }

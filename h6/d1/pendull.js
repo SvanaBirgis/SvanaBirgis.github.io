dce901ba3c325b6ec3361ec8c9b143392ce7a4cd
Svana Björg <100528784+SvanaBirgis@users.noreply.github.com>
2023-10-28 09:20:02 +0000

    Breyta nafni a kolli

---
+++ h6/d1/pendull.js
@@ -0,0 +1,43 @@
+var canvas;
+var gl;
+
+var numVertices  = 36;
+
+var movement = false;     // Do we rotate?
+var spinX = 0;
+var spinY = 0;
+var origX;
+var origY;
+
+var zDist = -3.0;
+
+var pointsArray = [];
+var colorArray = [];
+
+var vertices = [
+    vec4( -0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5,  0.5,  0.5, 1.0 ),
+    vec4(  0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5,  0.5, -0.5, 1.0 ),
+    vec4(  0.5, -0.5, -0.5, 1.0 )
+];
+
+var vertexColors = [
+    [0.0, 0.0, 0.0, 1.0],  // black
+    [1.0, 0.0, 0.0, 1.0],  // red
+    [1.0, 1.0, 0.0, 1.0],  // yellow
+    [0.0, 1.0, 0.0, 1.0],  // green
+    [0.0, 0.0, 1.0, 1.0],  // blue
+    [1.0, 0.0, 1.0, 1.0],  // magenta
+    [0.0, 1.0, 1.0, 1.0],  // cyan
+    [1.0, 1.0, 1.0, 1.0]   // white
+];
+
+
+// Stærð arma?
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
